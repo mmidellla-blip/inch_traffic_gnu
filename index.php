ed689c1848bac904eccde8d562b4123f2fe5e529
devgil3355 <devgil3355@gmail.com>
2025-10-27 11:36:55 +0900

    밑의 상담란에도 추가

--- index.php
+++ index.php
@@ -106,14 +106,14 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 							<input type="hidden" id="gclid_field" name="gclid_field" value="gclid_field">
 							<ul class="input-wrap">
 								<li><label for="h_tel">연락처</label><input class="sec07_input" type="tel" name="h_tel" id="h_tel" placeholder="연락처를 입력하세요." required/></li>
-								<li><label for="h_tel">지역</label><input class="sec07_input" type="text" name="h_place" id="h_place" placeholder="지역을 입력하세요." required/></li>
+								<li><label for="h_place">지역</label><input class="sec07_input" type="text" name="h_place" id="h_place" placeholder="지역을 입력하세요." required/></li>
 								<li class="full">
-									<label for="h_tel">분야</label>
+									<label for="wr_5">분야</label>
 									<select required="required" class="sec07_input" name="wr_5" id="wr_5">
 										<option>상담분야 선택</option>
-										<option>음주 행정심판</option>
-										<option>음주 형사처벌</option>
-										<option>일반 교통범죄</option>
+										<option value="음주 행정심판">음주 행정심판</option>
+										<option value="음주 형사처벌">음주 형사처벌</option>
+										<option value="일반 교통범죄">일반 교통범죄</option>
 									</select>
 								</li>
 								<li><input type="checkbox" id="chk" required/><label for="chk">개인정보 수집동의</label><a href="#">자세히보기</a></li>
@@ -244,16 +244,16 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 							<input type="hidden" id="gclid_field" name="gclid_field" value="gclid_field">
 							<ul class="input-wrap">
 								<li><label for="h_tel">연락처</label><input class="sec07_input" type="tel" name="h_tel" id="h_tel" placeholder="연락처를 입력하세요." required/></li>
+								<li><label for="h_place">지역</label><input class="sec07_input" type="text" name="h_place" id="h_place" placeholder="지역을 입력하세요." required/></li>
 								<li>
-									<label for="wr_5">분류</label>
-									<select CLASS="sec07_input" name="wr_5" id="wr_5" placeholder="분류를 입력하세요." required>
+									<label for="wr_5">분야</label>
+									<select CLASS="sec07_input" name="wr_5" id="wr_5" required>
 										<option>상담분야를 선택해 주세요.</option>
 										<option value="음주 행정심판">음주 행정심판</option>
 										<option value="음주 형사처벌">음주 형사처벌</option>
 										<option value="일반 교통범죄">일반 교통범죄</option>								
 									</select>
 								</li>
-								<li class="full"><label for="wr_content">내용</label><textarea name="wr_content" id="wr_content" cols="30" rows="10" placeholder="내용을 입력하세요."></textarea></li>
 								<li><input type="checkbox" id="chk" required/><label for="chk">개인정보 수집동의</label><a href="#">자세히보기</a></li>
 							</ul>
 							<p class="btn-wrap"><button id='submit_btn'>상담신청</button></p>
